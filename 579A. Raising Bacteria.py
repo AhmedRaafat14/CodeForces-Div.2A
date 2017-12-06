91a211ab09262ab6224a25dedfb746dfaf20c26d
Ahmad Ra'fat <ahmed.raafat1412@Gmail.com>
2017-12-06 23:22:43 +0200

    Create 579A. Raising Bacteria.py

---
+++ 579A. Raising Bacteria.py
@@ -0,0 +1,28 @@
+'''
+problem told us every single bacteria split into two each day
+so if we think for little bit we can find out 
+when number of bacteria in box being odd this mean he puts 
+a new one, so we count this one and we go throw till 
+number user gives us it being zero or less we use in this 
+integer division in python which give us integr value
+ when we made it like this:
+5 // 2 = 2
+2 // 2 = 1
+1 // 2 = 0
+    
+'''
+x = int ( input ( ) )
+
+sol = 0
+
+while x > 0:
+    if x % 2 == 1:
+        sol += 1
+
+    x //= 2
+
+print(sol)
+
+
+#Another approche: by counting number of ones in it which acctually exst in odd vaues
+#print( bin(int(input())).count('1') )
