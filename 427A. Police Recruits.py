9d8428713ffd569f6deb1716be4e6c70244f1ee9
Ahmad Ra'fat <ahmed.raafat1412@Gmail.com>
2017-12-31 19:58:01 +0200

    Add problem solution

--- 427A. Police Recruits.py
+++ 427A. Police Recruits.py
@@ -1,5 +1,54 @@
 #http://codeforces.com/contest/427/problem/A
+'''
+For this problem it's solution is so simple, just read
+problem statement carefually and you will got the answer:
 
+    Problem:
+        In this problem we want count the number of untrated crimes
+
+    Solution:
+        To simple one just by iterate over the list and check if the
+        current item is positive so this item is the number of hired
+        police officers, then get the next item and check if it's
+        negative so this is crime so check if we have police officers
+        free in hired or not if we have so hire one of them to this
+        crime and decrement it by one. Otherwise, we increment
+        the untrated counter by one because we have a crime and doesn't
+        have any free policeman.
+        sudocode:
+            Foreach el in range(li)
+                if el > 0:
+                    hired += el
+                    go to next item
+                if el < 0 and hired > 0:
+                    hired -= 1
+                    go to next item
+                if el < 0 and hired < 1:
+                    untrated += 1
+                    go to next item
+            print(untrated)
+
+
+    Example:
+        n  = 3
+        li = -1 -1 1
+        hired = untrated = 0
+
+        O(N)
+
+        Apply the prev sudocode descriped above:
+        foreach el in li:
+            el = -1 < 0 and hired < 1:
+                untrated += 1 ===> untrated = 1
+            el = -1 < 0 and hired < 1:
+                untrated += 1 ===> untrated = 2
+            el = 1 > 0:
+                hired += el ==> hired = 1
+            exit;
+
+        print(untrated)  ==>>  2
+
+'''
 n = int(input())
 
 li = list(map(int, input().split()))
