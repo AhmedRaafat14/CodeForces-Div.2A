1d97c726ee367d6c9c52536f2957cbe016687bd3
Ahmad Ra'fat <ahmed.raafat1412@Gmail.com>
2017-12-31 19:58:47 +0200

    change solution code

--- 427A. Police Recruits.py
+++ 427A. Police Recruits.py
@@ -56,13 +56,13 @@ li = list(map(int, input().split()))
 hired = untrated = 0
 
 for el in li:
-    if el == abs(el):
+    if el > 0:
         hired += el
         continue
-    if hired > 0 and el != abs(el):
+    if hired > 0 and el < 0:
         hired -= 1
         continue
-    if hired == 0 and el != abs(el):
+    if el < 0:
         untrated += 1
 
 print(untrated)
